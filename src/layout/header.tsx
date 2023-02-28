b7a1f9cfbae591caf714509df4b72d2d012fa443
thanhquisg24 <thanhquisg24@gmail.com>
2023-02-28 12:16:14 +0700

    Fix header

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { isMobile } from "react-device-detect";
 
 import ConnectWalletContainer from "./ConnectWalletContainer";
 
@@ -8,16 +9,20 @@ export default function Header() {
       <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
         <a href="#" className="logo d-flex align-items-center">
           <img src="/static/img/logo/icon-figchain.png" alt="Fig" />
-          <span className="text-theme-1">FIG NFT</span>
+          {isMobile === false && <span className="text-theme-1">FIG NFT</span>}
         </a>
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li>
-              <ConnectWalletContainer />
-            </li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle" />
-        </nav>
+        {isMobile ? (
+          <ConnectWalletContainer />
+        ) : (
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li>
+                <ConnectWalletContainer />
+              </li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle" />
+          </nav>
+        )}
         {/* .navbar */}
       </div>
     </header>
